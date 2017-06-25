992e4ce241e92eab9d4b5cc3f9d1907564dd5bb6
Adapter668 <natalia.braniewska@gmail.com>
2017-06-26 01:57:38 +0200

    Add photo to database and from database to canvas

--- Medical-teleconference/Medical-teleconference/Controllers/ConferenceController.cs
+++ Medical-teleconference/Medical-teleconference/Controllers/ConferenceController.cs
@@ -1,6 +1,7 @@
 ﻿using Medical_teleconference.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,12 +10,67 @@ namespace Medical_teleconference.Controllers
 {
     public class ConferenceController : Controller
     {
+        private TeleconferenceDbContext db = new TeleconferenceDbContext();
+
         //
         // GET: /Conference/
 
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
-            return View();
+            Room room = db.Rooms.Find(id);
+            ViewBag.RoomName = room.RoomName;
+            ViewBag.RoomId = id;
+            
+            List<Photo> photosInRoom = new List<Photo>();
+
+            foreach (Photo p in db.Photos)
+            {
+                if(p.RoomId == id)
+                {
+                    photosInRoom.Add(p);
+                }
+            }
+
+            return View(photosInRoom);
+        }
+
+        public ActionResult Show(int? id)
+        {
+            Photo p = db.Photos.Find(id);
+            string b64 = Convert.ToBase64String(p.photo);
+            string content = "data:" + p.MimeType + ";base64," + b64;
+
+            return Content(content);
+        }
+
+        //[HttpPost]
+        public ActionResult Upload(HttpPostedFileBase image, string RoomId)
+        {
+            string pic = System.IO.Path.GetFileName(image.FileName);
+            string type = pic.Split('.').Last();
+            string mime = "image/" + type;
+
+            if (image != null)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    image.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+
+                    int i = db.Photos.Count();
+                    Photo photo = new Photo();
+                    photo.PhotoId = i;
+                    photo.RoomId = int.Parse(RoomId);
+                    photo.photo = array;
+                    photo.MimeType = mime;
+
+                    db.Photos.Add(photo);
+                    db.SaveChanges();
+                }
+
+            }
+
+            return RedirectToAction("Index", new { id = int.Parse(RoomId)});
         }
 
     }
---
+++ Medical-teleconference/Medical-teleconference/Scripts/ImageUpload.js
@@ -0,0 +1,77 @@
+﻿// jqXHRData needed for grabbing by data object of fileupload scope
+var jqXHRData;
+
+$(function () {
+
+	//Initialization of fileupload
+	initSimpleFileUpload();
+
+	//Handler for "Start upload" button 
+	$("#form0").on('submit', function () {
+		console.log("Submit kot");
+		if (jqXHRData) {
+			jqXHRData.submit();
+		}
+		return false;
+	});
+
+	$("#imageList a").on('click', function () {
+		console.log("clicked", $(this));
+		getImage($(this).attr("data"))
+			.done(function (res) {
+				drawDownloadedImage(res);
+			})
+			.fail(function () {
+				console.log(":'(");
+			});
+		return false;
+	});
+});
+
+function initSimpleFileUpload() {
+	'use strict';
+
+	console.log($('#imageFile'));
+
+	$('#imageFile').fileupload({
+		url: '/Conference/Upload',
+		dataType: 'json',
+		add: function (e, data) {
+			jqXHRData = data
+		},
+		done: function (event, data) {
+			if (data.result.isUploaded) {
+
+			}
+			else {
+
+			}
+			alert(data.result.message);
+		},
+		fail: function (event, data) {
+			if (data.files[0].error) {
+				alert(data.files[0].error);
+			}
+		}
+	});
+}
+
+function getImage(id) {
+	var url = `/Conference/Show/${id}`;
+
+	return $.get(url);
+}
+
+function drawDownloadedImage(imageSrc) {
+	var canvas = document.getElementById("imageToTransform");
+	var context = canvas.getContext("2d");
+	var img = new Image();
+
+	img.onload = function () {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.save();
+		context.drawImage(img, 0, 0);
+	}
+	img.src = imageSrc;
+	$("#urlWhy").text(imageSrc);
+}
