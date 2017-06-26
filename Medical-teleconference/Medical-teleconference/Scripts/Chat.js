b12f7528cecc913325ca41e132d0ee0899d71ac7
Adapter668 <natalia.braniewska@gmail.com>
2017-06-26 03:36:01 +0200

    simple chat with node.js server

---
+++ Medical-teleconference/Medical-teleconference/Scripts/Chat.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+	var socket = io('http://localhost:3000');
+
+	var channel = $(this).find("#roomName").text();
+	console.log('Changing channel to', channel);
+	if (channel) {
+		socket.emit('join', channel);
+	}
+
+	$('form').submit(function () {
+		var msg = $('#m').val();
+
+		console.log('Sending message', msg)
+		socket.emit('msg', msg);
+		$('#m').val('');
+		return false;
+	});
+
+	socket.on('msg', function (msg) {
+		$('#messages').append($('<li>').text(msg));
+	});
+});
